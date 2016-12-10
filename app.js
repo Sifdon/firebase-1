26521587a89e2b5afb13ae5c96fecf6b64306014
Daniel hernandez <danielhernandez16@hotmail.com>
2016-12-10 14:29:52 -0500

    sync child events for list in real-time

--- app.js
+++ app.js
@@ -11,13 +11,33 @@
 
 	// obtener elementos
 	const preObject = document.getElementById('objeto')
+	const ulList = document.getElementById('lista')
 
 	// crear referencias a DB
 	const dbRefObject = firebase.database().ref().child('objeto')
+	const dbRefList = dbRefObject.child('habilidades')
 
 	// sincronizar cambios objeto con metodo on
 	dbRefObject.on('value', snap => {
 		preObject.innerText = JSON.stringify(snap.val(), null, 3)
+	})
+
+	// sincronizar cambios lista
+	dbRefList.on('child_added', snap => {
+		const li = document.createElement('li')
+		li.innerText = snap.val()
+		li.id = snap.key
+		ulList.appendChild(li)
+	})
+
+	dbRefList.on('child_changed', snap => {
+		const liChanged = document.getElementById(snap.key)
+		liChanged.innerText = snap.val()
+	})
+
+	dbRefList.on('child_removed', snap => {
+		const liToRemove = document.getElementById(snap.key)
+		liToRemove.remove()
 	})
 
 } ())
